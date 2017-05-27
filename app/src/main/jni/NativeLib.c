2f6eb142142b9664415649710d06981d1e5caeb1
Rikharthu <uberviolence@gmail.com>
2017-05-27 20:26:06 +0300

    added method table

--- app/src/main/jni/NativeLib.c
+++ app/src/main/jni/NativeLib.c
@@ -1,13 +1,13 @@
 #include "NativeLib.h"
 #include <cpu-features.h>
 
-JNIEXPORT jint JNICALL Java_com_example_uberv_nativeapp_NativeLib_getCpuCount
+JNIEXPORT jint JNICALL getCpuCount
         (JNIEnv *env, jclass clazz) {
     // use method from cpu-features.h
     return android_getCpuCount();
 }
 
-JNIEXPORT jstring JNICALL Java_com_example_uberv_nativeapp_NativeLib_getCpuFamily
+JNIEXPORT jstring JNICALL getCpuFamily
         (JNIEnv *env, jclass clazz) {
     AndroidCpuFamily family = android_getCpuFamily();
     switch (family) {
@@ -26,5 +26,28 @@ JNIEXPORT jstring JNICALL Java_com_example_uberv_nativeapp_NativeLib_getCpuFamil
         case ANDROID_CPU_FAMILY_UNKNOWN:
         default:
             return (*env)->NewStringUTF(env, "Vaporware");
+    }
+}
+
+//Construct a table mapping Java method signatures to native function pointers
+static JNINativeMethod method_table[] = {
+        {"getCpuCount",  "()I",                  (void *) getCpuCount},
+        {"getCpuFamily", "()Ljava/lang/String;", (void *) getCpuFamily}
+};
+
+// This is invoked when the library is loaded (System.loadLibrary() in Java side)
+//Use the OnLoad initializer to register the method table with the runtime
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env;
+    if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_6) != JNI_OK) { return JNI_ERR; }
+    else {
+        jclass clazz = (*env)->FindClass(env, "com/example/uberv/nativeapp/NativeLib");
+        if (clazz) {
+            jint ret = (*env)->RegisterNatives(env, clazz, method_table,
+                                               sizeof(method_table) /
+                                               sizeof(method_table[0]));
+            if (ret == 0) { return JNI_VERSION_1_6; }
+        }
+        return JNI_ERR;
     }
 }
